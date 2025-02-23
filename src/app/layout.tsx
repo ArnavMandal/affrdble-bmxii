29159a9bd02d45a74b34dc44a209f7a9a5b8ec58
Shriyan Bachigari <shriyanbachigari@gmail.com>
2025-02-23 08:07:55 -0500

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar";
-const inter = Inter();
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"], // Specify the font weights you want to use
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
